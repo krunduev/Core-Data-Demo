f063de2b41c1a00c40e4a85b732bb67b3c4faa9a
Kostyantyn Runduyev <krunduev@curiousit.eu>
2016-11-27 15:18:19 +0200

    Updates

--- Core-Data-Demo/ViewController.swift
+++ Core-Data-Demo/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("Rob", forKey: "name")
+        newUser.setValue("myPass", forKey: "password")
+        newUser.setValue(35, forKey: "age")
+        
+        do {
+            
+            try context.save()
+            print("saved")
+            
+        } catch {
+            
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let username = result.value(forKey: "name") as? String {
+                        print(username)
+                    }
+                    
+                }
+                
+            } else {
+                print("No results")
+            }
+            
+        } catch {
+            print("Could not fetch results")
+        }
     }
 
     override func didReceiveMemoryWarning() {
